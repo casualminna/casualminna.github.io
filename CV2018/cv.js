06723287bfe5882de68f855d5ea85c33f42dff49
casualminna <casualminna@users.noreply.github.com>
2020-10-22 10:04:22 +0300

    CV added

---
+++ CV2018/cv.js
@@ -0,0 +1,36 @@
+
+function myFunctionHaaga() {
+  var x = document.getElementById("HaagaHide");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionMetropolia() {
+  var x = document.getElementById("MetropoliaHide");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionIffOld() {
+  var x = document.getElementById("IffNewHide");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionIffNew() {
+  var x = document.getElementById("IffOldHide");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
